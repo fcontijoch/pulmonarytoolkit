63017f081d6639ad47d7b4cc91e34ca99cfc6cec
Pranav Sujitkumar <Pranav@Pranavs-MacBook-Pro.local>
2016-11-20 12:54:17 -0800

    Tried to add code to run vessel segmentation on full image, did not work. Needs fixing. Changed TEST_Script to be able to save the images correctly. Next step is to add perfusion images to the script and run the airway/lungs/vessels for all the datasets using PTK methods.

--- Scripts/UCSD_Scripting_Example.m
+++ Scripts/UCSD_Scripting_Example.m
@@ -3,7 +3,7 @@ PTKAddPaths;
 %%
 ptk_main = PTKMain;
 
-source_path = 'C:\Users\Jacqueline\Dropbox\CTEPH\MATLAB code\Datasets\CTEPH_0008\97272748\03713398';
+source_path = '/Users/Pranav/Documents/CTEPH_DATASETS/CTEPH_0008/orig_imgs';
 file_infos = PTKDicomUtilities.GetListOfDicomFiles(source_path);
 % Tutorial 3 says PTKDiskUtilities which is incorrect
 
@@ -20,11 +20,24 @@ vessels_big = dataset.GetResult('PTKVesselness');
 %%
 reporting = ptk_main.ReportingWithCache;
 %%
-PTKVisualiseIn3D([],vessels2,4,false,reporting);
+%this does not work: must fix
+%PTKVisualiseIn3D([],vessels2,4,false,reporting);
+%%
+
+%%
+% Please remove cache when re-running results
+dataset.DeleteCacheForThisDataset;
+vessels_all = dataset.GetResult('PTKVesselness');
 %%
 PTKViewer(lungs);
 %%
 PTKViewer(vessels_big);
 
 %%
-PTKSaveAs(lobes, 'Patient Name', '/Users/fcontijoch/Documents/UCSD/Images/PCTA/CTEPH_0008/',true);
+%when saving use filter index 0. 
+PTKSaveAs(lungs, 'Patient Name', '/Users/Pranav/Documents/CTEPH_DATASETS/CTEPH_0008/',0,reporting);
+
+%%
+PTKSaveAs(vessels, 'Patient Name', '/Users/Pranav/Documents/CTEPH_DATASETS/CTEPH_0008/',0,reporting);
+%%
+PTKSaveAs(vessels_all, 'Patient Name', '/Users/Pranav/Documents/CTEPH_DATASETS/CTEPH_0008/',0,reporting);
