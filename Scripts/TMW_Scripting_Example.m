01a2c7ec3ceb1358f0f90438567c352daddaf178
Terrence Wong <tmw008@ucsd.edu>
2017-01-23 14:41:51 -0800

    TMW script trial

--- Scripts/TMW_Scripting_Example.m
+++ Scripts/TMW_Scripting_Example.m
@@ -24,12 +24,19 @@ lungs = dataset.GetResult('PTKLeftAndRightLungs');
 lobes = dataset.GetResult('PTKLobes');
 vessels = dataset.GetResult('PTKVesselness');
 vessels2 = dataset.GetResult('PTKVesselnessDilated');
-
+%%
+PTKViewer(lungs)
 %% Trying to dilate rightlungROI and leftlungROI in order to add additional parts of lungs into the image 
-lungs = dataset.GetResult('PTKLeftAndRightLungs');
+whos 'source_path'
+whos 'lungs'
+class lungs
+%%
 %rollingball adds a nonflat ball-shaped structure element 
 %can also use vertical line element strel('line', 11, 90)
+%changing the data from char to logical to be used in dilation
+IM= imread('lungs')
 
+%%
 rollingball= offsetstrel('ball', 5,5); 
 %imdilate must be used on a uint8 or logical, data must be flat and 2D 
 dilatedRightLeftLungROI=imdilate(lungs, rollingball, 'dilate', Dicom)
